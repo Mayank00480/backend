3bd556cc3068cd67991c7cbdf9aff7086621d933
Mayank00480 <mayanksharma1937@gmail.com>
2024-01-27 12:44:16 +0530

    form added with the functionality mentioned

---
+++ Form/app.js
@@ -0,0 +1,48 @@
+const http = require('http')
+const fs = require('fs');
+http.createServer((req,res) => {
+    if(req.url === '/'){
+        let readData = fs.readFileSync('./message.txt');
+        res.setHeader('Content-Type','text/html');
+        if(readData != undefined)
+        {
+            
+            res.write(`<p>${readData}</p>`)
+            
+        }
+       
+        res.write('<form action = "/message" method = "POST"><input name = "message"> <button type = "submit">send</button></form>')
+        
+        res.end();
+    }
+     if(req.url === '/message'){
+         const body = [];
+         req.on('data',(chunk) => {
+             body.push(chunk);
+         })
+         req.on('end',()=>{
+             const parsedData = Buffer.concat(body).toString();
+             const realData = parsedData.split('=')[1];
+             fs.writeFile('message.txt',realData,()=>{
+
+
+                res.writeHead(302, {
+                    'Location': '/', // Specify the new path for redirection
+                  });
+                  res.end();
+             });
+            
+            
+         } )
+       
+     }
+
+// For Redirect
+    // if(req.url === '/message'){
+    //     fs.writeFileSync('./message.txt', 'Hello');
+    //     res.statusCode = 302;
+    //     res.setHeader('Location' , '/');
+    //     res.end();
+    // }
+
+}).listen(8000);
